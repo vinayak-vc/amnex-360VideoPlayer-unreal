ae93886290ea26a60c0dd4e2082716f89614b212
vinayak-vc <vinayak.lakhani@viitor.cloud>
2026-06-17 17:36:43 +0530

    feat: integrate point count limit slider, fix standalone builds, and commit UI/level updates

---
+++ Content/UnrealClaude/Scripts/Check_ConvertPanelClass_on_lev_010.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6994bbda62eb5c4a36665b871aca0073a27e7aba9e75f126cd9745765a7ab41c
+size 629
---
+++ Content/UnrealClaude/Scripts/Run_fixed_billboard_material_b_006.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee754af686b1ab333f5f143a6185c50b59a92e03b62cc09f1ef4013540368651
+size 143
